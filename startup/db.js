acba7d0ad417a38a6885b28707750c3a3bb21a69
Augustine Awuori <augustineawuori95@gmail.com>
2022-06-30 19:17:22 +0300

    Log connected DB.

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 module.exports = function () {
   mongoose
     .connect(`mongodb://localhost/${process.env.DB}`)
-    .then(() => console.log("Connected to the MongoDB"))
-    .catch((err) => console.error("Connection to the MongoDB falied", err));
+    .then(() => console.log(`Connected to the ${process.env.DB} DB`))
+    .catch((err) =>
+      console.error(`Connection to the ${process.env.DB} db failed`, err)
+    );
 };
