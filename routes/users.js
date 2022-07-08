f580e83f86212a190498332985de5ee6c1d78ea3
Augustine Awuori <augustineawuori95@gmail.com>
2022-07-08 23:35:56 +0300

    Register a new user.

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,35 @@
-const validator = require("../middleware/validate");
-const { User, validate } = require("../models/user");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
+const multer = require("multer");
 const express = require("express");
 const router = express.Router();
 
-router.post("/", validator(validate), async (req, res) => {
+const validator = require("../middleware/validate");
+const { User, validate } = require("../models/user");
+const avatarResize = require("../middleware/avatarResize");
+
+const upload = multer({ dest: "uploads/" });
+
+router.post(
+  "/",
+  [upload.single("avatar"), avatarResize, validator(validate)],
+  async (req, res) => {
     let user = await User.findOne({ username: req.body.username });
     if (user)
-        return res.status(400).send("A user with the username already exist.");
+      return res.status(400).send("A user with the username already exist.");
 
-    user = new User(req.body, _.pick(["name", "username", "password"]));
+    user = new User(_.pick(req.body, ["name", "username", "password"]));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
+    if (req.avatar) user.avatar = req.avatar;
+
     await user.save();
 
     res
-        .header("x-auth-token", user.generateAuthToken())
-        .send(_.pick(user, ["_id", "name", "username", "isAdmin", "isVerified"]));
-});
+      .header("x-auth-token", user.generateAuthToken())
+      .header("access-control-expose-headers", "x-auth-token")
+      .send(_.pick(user, ["_id", "name", "username", "isAdmin", "isVerified"]));
+  }
+);
 
 module.exports = router;
