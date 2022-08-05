7327a33bd54c997955e2d2c78d256be76f94af9e
Augustine Awuori <codewithaugustine@gmail.com>
2022-08-05 03:28:42 +0300

    Fix the bug that give out the right avatar urls.

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const _ = require("lodash");
 
 const { imageMapper } = require("../mappers/listings");
 const { Post } = require("../models/post");
+const { User } = require("../models/user");
 const auth = require("../middleware/auth");
 const imageResize = require("../middleware/imageResize");
 const validateUser = require("../middleware/validateUser");
@@ -19,12 +21,12 @@ router.post(
   [auth, upload.array("images"), validateUser, imageResize, validatePost],
   async (req, res) => {
     let post = new Post({
-      author: {
-        _id: req.user._id,
-        name: req.user.name,
-        username: req.user.username,
-        avatar: req.user.avatar,
-      },
+      author: _.pick(await User.findById(req.user._id), [
+        "_id",
+        "avatar",
+        "name",
+        "username",
+      ]),
       message: req.body.message,
     });
     post.images = req.images.map((fileName) => ({ fileName }));
