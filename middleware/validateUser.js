35b3e50d069f27a731307256bbeadd2d6fcb42fd
Augustine Awuori <codewithaugustine@gmail.com>
2022-08-06 22:32:56 +0300

    Reduce query by adding user to 'req.user'.

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,9 +1,11 @@
 const { User } = require("../models/user");
+const _ = require("lodash");
 
 module.exports = async (req, res, next) => {
   const user = await User.findById(req.user._id);
 
   if (!user) return res.status(400).send({ error: "Invalid user" });
+  req.user = _.pick(user, ["_id", "name", "username", "avatar", "aboutMe"]);
 
   next();
 };
