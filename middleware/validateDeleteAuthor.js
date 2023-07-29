a56f1307e1d4409e0aa32c478353fd87fdf346d3
augustine-awuori <codewithaugustine@gmail.com>
2023-07-29 19:32:17 +0300

    Fix the bug that prevented the web user from deleting a listing.

--- middleware/validateDeleteAuthor.js
+++ middleware/validateDeleteAuthor.js
@@ -3,10 +3,11 @@ const { Listing } = require("../models/listing");
 module.exports = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
-  if (req.user._id !== listing.author._id.valueOf())
+  if (req.user._id !== listing.authorId.valueOf())
     return res.status(401).send({
       error: "Permisson denied. You're not the author of this listing.",
     });
 
+  req.listing = listing;
   next();
 };
