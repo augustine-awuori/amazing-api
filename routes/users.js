24a548b602698c6df3860b8ce0d9378bdb6a545b
augustine-awuori <codewithaugustine@gmail.com>
2023-07-31 09:27:20 +0300

    Fix the bug that broke the app when accessing user with invalid id.

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,19 @@ const bcrypt = require("bcrypt");
 const multer = require("multer");
 const express = require("express");
 const router = express.Router();
+const { isValidObjectId } = require("mongoose");
 
-const { imageUnmapper } = require("../mappers/images");
 const { mapUser, mapUsers } = require("../mappers/users");
 const { User, validate } = require("../models/user");
-const { Listing } = require("../models/listing");
 const auth = require("../middleware/auth");
 const avatarResize = require("../middleware/imageResize");
 const imagesResize = require("../middleware/imagesResize");
 const validateUser = require("../middleware/validateUser");
 const validator = require("../middleware/validate");
-const { isValidObjectId } = require("mongoose");
 
 const upload = multer({ dest: "uploads/" });
+
+const IMAGES_COUNT = 2;
 
 router.post(
   "/",
@@ -48,7 +48,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const userId = req.params.id;
-  if (!isValidObjectId) return res.status(400).send({ error: "Invalid ID." });
+  if (!isValidObjectId(userId)) return res.status(400).send({ error: "Invalid ID." });
 
   const user = await User.findById(userId);
   if (!user)
@@ -61,93 +61,28 @@ router.get("/:id", async (req, res) => {
 
 router.patch(
   "/",
-  [auth, validateUser, upload.array("images"), imagesResize],
+  [auth, validateUser, upload.array("images", IMAGES_COUNT), imagesResize],
   async (req, res) => {
+    console.log(req.files);
     const { aboutMe, name, instagram, twitter, whatsapp, username } = req.body;
     const user = await User.findById(req.user._id);
 
     if (aboutMe) user.aboutMe = aboutMe;
     if (user.username !== username) {
       const userByUsername = await User.findOne({ username });
-
       if (userByUsername)
-        return res.status(400).send("The username is already taken.");
+        return res
+          .status(400)
+          .send({ error: "The username is already taken." });
     }
     user.name = name;
     user.username = username;
     user.otherAccounts = { whatsapp, instagram, twitter };
 
-    for (let image of req.files) {
-      const name = image.originalname;
-      if (user[name]) imageUnmapper(user[name]);
-      user[name] = {};
-      user[name].fileName = image.filename;
-    }
-
-    await updateUserItems(req.user, user);
     await user.save();
 
     res.send({ token: user.generateAuthToken(), user: mapUser(user) });
   }
 );
-
-async function updateUserItems(originalUser, newUser) {
-  updateListAuthor(Post, originalUser, newUser);
-  updateListAuthor(Listing, originalUser, newUser);
-}
-
-async function updateListAuthor(ListModel, originalUser, newUser) {
-  const author = pickEssentialProps(originalUser);
-  const list = await ListModel.find({ author });
-  const updated = pickEssentialProps(newUser);
-
-  list.forEach((item) => {
-    item.author = updated;
-    item.save();
-  });
-}
-
-function pickEssentialProps(user) {
-  return user
-    ? _.pick(user, ["_id", "aboutMe", "avatar", "name", "username"])
-    : user;
-}
-
-async function handleFollowingsAndFollowers(follower, leaderId) {
-  if (!leaderId) return;
-  const leader = await User.findById(leaderId);
-  if (!leader) return;
-
-  await handleFollowerFollowings(leader, follower);
-  return await handleLeaderFollowers(leader, follower);
-}
-
-async function handleLeaderFollowers(leader, follower) {
-  const index = findIndex(leader.followers, follower);
-
-  if (exists(index)) leader.followers.splice(index, 1);
-  else leader.followers.unshift(pickEssentialProps(follower));
-
-  await leader.save();
-
-  return leader;
-}
-
-async function handleFollowerFollowings(leader, follower) {
-  const index = findIndex(follower.followings, leader);
-
-  if (exists(index)) follower.followings.splice(index, 1);
-  else follower.followings.unshift(pickEssentialProps(leader));
-
-  await follower.save();
-}
-
-function findIndex(list = [], item) {
-  return list.findIndex((i) => i._id.toString() === item._id.toString());
-}
-
-function exists(index = -1) {
-  return index !== -1;
-}
 
 module.exports = router;
