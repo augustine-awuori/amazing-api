9bca0af35b03ccba9c6ae48fd97b506a970d3398
augustine-awuori <codewithaugustine@gmail.com>
2023-08-04 06:36:58 +0300

    Extract validation logic.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const winston = require("winston");
 const express = require("express");
 const app = express();
 
@@ -5,13 +6,11 @@ require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
-const server = app
-  .listen(port, () => console.log(`Listening on port ${port}...`))
-  .on("error", () => {
-    process.once("SIGUSR2", () => process.kill(process.pid, "SIGUSR2"));
-    process.once("SIGINT", () => process.kill(process.pid, "SIGINT"));
-  });
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
 module.exports = server;
