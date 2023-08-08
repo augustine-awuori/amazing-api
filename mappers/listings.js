9340af60becea0630f2c4d7c31619bc260f04ce8
augustine-awuori <codewithaugustine@gmail.com>
2023-08-08 07:45:52 +0300

    Update user images.

--- mappers/listings.js
+++ mappers/listings.js
@@ -1,10 +1,6 @@
-const fs = require("fs");
-
 const { Category } = require("../models/category");
 const { mapImage } = require("../utility/imageManager");
 const { User } = require("../models/user");
-
-const outputFolder = "public/assets/";
 
 const mapListing = async (listing) => {
   const author = await User.findById(listing.authorId);
@@ -21,18 +17,9 @@ const mapListing = async (listing) => {
 const mapListings = async (listings) =>
   await Promise.all(listings.map(mapListing));
 
-const imageUnmapper = (listing) =>
-  listing.images.forEach(async (image) => {
-    if (image?.fileName) {
-      fs.unlinkSync(`${outputFolder}${image.fileName}_full.jpg`);
-      fs.unlinkSync(`${outputFolder}${image.fileName}_thumb.jpg`);
-    }
-  });
-
 const mapAvatar = (avatarUrl) => (avatarUrl ? mapImage(avatarUrl) : avatarUrl);
 
 module.exports = {
-  imageUnmapper,
   mapAvatar,
   mapListing,
   mapListings,
