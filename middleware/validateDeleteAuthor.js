db678578b6bfcb81df5e937369717cb50770c6df
augustine-awuori <codewithaugustine@gmail.com>
2023-08-29 16:55:32 +0300

    Fix the bug that prevented the user from editing a listing.

--- middleware/validateDeleteAuthor.js
+++ middleware/validateDeleteAuthor.js
@@ -1,13 +1,8 @@
-const { Listing } = require("../models/listing");
-
 module.exports = async (req, res, next) => {
-  const listing = await Listing.findById(req.params.id);
-
-  if (req.user._id !== listing.authorId.valueOf())
+  if (req.user._id !== req.listing.author.toString())
     return res.status(401).send({
       error: "Permisson denied. You're not the author of this listing.",
     });
 
-  req.listing = listing;
   next();
 };
