eb8f835e88a118e6aab38074a39bf822c82b8896
augustine-awuori <codewithaugustine@gmail.com>
2023-08-29 16:55:32 +0300

    Fix the bug that prevented the user from editing a listing.

--- middleware/validateListingAuthor.js
+++ middleware/validateListingAuthor.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res, next) => {
-  if (req.listing.authorId.valueOf() !== req.body.authorId)
+  if (req.listing.author.toString() !== req.body.authorId)
     return res.status(401).send({ error: "You're not the listing author" });
 
   next();
