a55f8c00642e0bdebebd1fdc48c4e4b3049c02bd
augustine-awuori <codewithaugustine@gmail.com>
2023-09-22 17:43:20 +0300

    Fix the bug that prevented the products from being updated

--- middleware/validateProductAuthor.js
+++ middleware/validateProductAuthor.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res, next) => {
-  if (req.product.author.toString() !== req.body.author.toString())
+  if (req.product.author.toString() !== req.user._id.toString())
     return res.status(401).send({ error: "You're not the listing author" });
 
   next();
--- middleware/validateProductId.js
+++ middleware/validateProductId.js
@@ -1,10 +1,10 @@
 const service = require("../services/products");
 
 module.exports = async (req, res, next) => {
-  const product = await service.findById(req.body._id);
+  const product = await service.findById(req.params.id);
 
   if (!product)
-    return res.status(400).send({ error: "Product is already deleted." });
+    return res.status(400).send({ error: "Product is not in database." });
   req.product = product;
 
   next();
