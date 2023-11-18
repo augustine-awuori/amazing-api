d0ef12845ce4e67baf0804cf3ea21addc1155007
augustine-awuori <codewithaugustine@gmail.com>
2023-11-18 15:56:03 +0300

    Bug fix: allow author to delete his request

--- middleware/validateRequestAuthor.js
+++ middleware/validateRequestAuthor.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
   const request = await Request.findById(req.params.id);
   if (!request) return res.status(404).send({ error: "Request not found!" });
 
-  if (request.author.toString() !== req.body.author.toString())
+  if (request.author.toString() !== req.user._id.toString())
     return res.status(403).send({ error: "Forbidden! You're not the author!" });
 
   req.request = request;
