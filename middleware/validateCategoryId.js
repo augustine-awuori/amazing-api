7b4e571b818a3c645450544d1d2e979a9aa2d620
augustine-awuori <codewithaugustine@gmail.com>
2024-01-25 17:35:23 +0300

    Improve the logic

--- middleware/validateCategoryId.js
+++ middleware/validateCategoryId.js
@@ -5,11 +5,11 @@ const getCategory = require("../utility/getCategory");
 module.exports = async (req, res, next) => {
   const categoryId = req.body.category;
   if (!isValidObjectId(categoryId))
-    return res.status(400).send({ error: "Invalid ID" });
+    return res.status(400).send({ error: "Invalid category ID" });
 
   const category = await getCategory(categoryId);
 
-  if (!category) return res.status(400).send({ error: "Invalid category." });
+  if (!category) return res.status(404).send({ error: "Category don't exist" });
   req.category = category;
 
   next();
