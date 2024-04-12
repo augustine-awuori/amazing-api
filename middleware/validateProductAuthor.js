8f895ef9ce97be8524cf61c01edf63ea9bceb88e
augustine-awuori <codewithaugustine@gmail.com>
2024-04-12 10:59:42 +0300

    Fix the bug that prevented the user from updating a product

--- middleware/validateProductAuthor.js
+++ middleware/validateProductAuthor.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
   if (!seller?._id?.toString())
     return res.status(500).send({ error: "App error!" });
 
-  if (seller._id.toString() !== req.user._id.toString() || !req.user.isAdmin)
+  if (seller._id.toString() !== req.user._id.toString() && !req.user.isAdmin)
     return res.status(403).send({ error: "You're not authorised" });
 
   next();
