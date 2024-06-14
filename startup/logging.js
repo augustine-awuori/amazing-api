9c381f6581631d48c72b6582c149fe5ca73338b3
augustine-awuori <codewithaugustine@gmail.com>
2024-06-15 00:54:27 +0300

    Handle logging exceptions

--- startup/logging.js
+++ startup/logging.js
@@ -3,27 +3,36 @@ require("winston-mongodb");
 require("express-async-errors");
 
 module.exports = function () {
-  winston.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    })
-  );
-
-  new winston.Logger({
+  // Configure the default logger with console and file transports
+  winston.configure({
     transports: [
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      }),
+      new winston.transports.File({ filename: "logfile.log" }),
+    ],
+    exceptionHandlers: [
       new winston.transports.File({
-        handleExceptions: true,
-        handleRejections: true,
         filename: "uncaughtExceptions.log",
+        handleExceptions: true,
+      }),
+    ],
+    rejectionHandlers: [
+      new winston.transports.File({
+        filename: "unhandledRejections.log",
+        handleRejections: true,
       }),
     ],
   });
 
+  // Handle unhandled promise rejections
   process.on("unhandledRejection", (ex) => {
+    winston.error(ex.message, ex);
     throw ex;
   });
 
-  winston.configure({
-    transports: [new winston.transports.File({ filename: "logfile.log" })],
+  // Handle uncaught exceptions
+  process.on("uncaughtException", (ex) => {
+    winston.error(ex.message, ex);
   });
 };
