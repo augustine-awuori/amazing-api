06add74365ecfa1abd9e4b73b9ee20683bb9d85b
augustine-awuori <codewithaugustine@gmail.com>
2024-06-27 15:46:56 +0300

    Bug fix: show process

--- routes/auth.js
+++ routes/auth.js
@@ -13,17 +13,20 @@ router.post("/", validator(validate), async (req, res) => {
   if (!user) return res.status(404).send({ error: "Email isn't registered." });
   console.log("user.password", user.password);
   if (user.password) {
+    console.log("comparing passwords");
     const isValidPassword = bcrypt.compare(password, user.password);
     if (!isValidPassword)
       return res
         .status(400)
         .send({ error: "Invalid username and/or password." });
   } else {
+    console.log("processing and saving password");
     const salt = await bcrypt.genSalt(10);
     user.password = bcrypt.hash(password, salt);
     await user.save();
   }
 
+  console.log("preparing and sending token");
   const token = user.generateAuthToken();
   res.send(token);
 });
