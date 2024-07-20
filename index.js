599c67dd5968c900b3648b655291a892895531de
augustine-awuori <codewithaugustine@gmail.com>
2024-07-21 00:14:12 +0300

    Restore app

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const winston = require("winston");
 const express = require("express");
+const app = express();
 
-let server;
+require("./startup/logging");
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
-function initializeServer() {
-  if (server) return server;
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
-  const app = express();
-
-  require("./startup/logging");
-  require("./startup/routes")(app);
-  require("./startup/db")();
-  require("./startup/config")();
-  require("./startup/validation")();
-  require("./startup/prod")(app);
-
-  const port = process.env.PORT || 3000;
-  server = app.listen(port, () =>
-    winston.info(`Listening on port ${port}...`)
-  );
-
-  return server;
-}
-
-module.exports = initializeServer;
+module.exports = server;
