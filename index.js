a340f65569a67d7ce19d31408fdf6debbb399e56
augustine-awuori <codewithaugustine@gmail.com>
2024-07-21 00:04:53 +0300

    Ensure that the server initialization logic is executed only once

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const winston = require("winston");
 const express = require("express");
-const app = express();
 
-require("./startup/logging");
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
-require("./startup/prod")(app);
+let server;
 
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
-);
+function initializeServer() {
+  if (server) return server;
 
-module.exports = server;
+  const app = express();
+
+  require("./startup/logging");
+  require("./startup/routes")(app);
+  require("./startup/db")();
+  require("./startup/config")();
+  require("./startup/validation")();
+  require("./startup/prod")(app);
+
+  const port = process.env.PORT || 3000;
+  server = app.listen(port, () =>
+    winston.info(`Listening on port ${port}...`)
+  );
+
+  return server;
+}
+
+module.exports = initializeServer;
