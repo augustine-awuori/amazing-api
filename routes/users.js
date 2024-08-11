2914289c12a405a0ab0d1c9339350b802923cf59
augustine-awuori <codewithaugustine@gmail.com>
2024-08-11 12:40:43 +0300

    Update all user's feed token

--- routes/users.js
+++ routes/users.js
@@ -48,14 +48,14 @@ router.post("/quick", validator(validate), async (req, res) => {
 router.get("/", async (_req, res) => {
   const users = await service.getAll();
 
-  res.send(users);
-
   const allUsers = await User.find({});
   allUsers.forEach(async user => {
     if (user.username !== "@augustine" && !user.feedToken) {
       await service.findByIdAndUpdate(user._id, { feedToken: service.getUserFeedToken(user._id) });
     }
   });
+
+  res.send(users);
 });
 
 router.get("/:id", async (req, res) => {
