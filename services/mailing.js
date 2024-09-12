609436683a9787dbd22d9671d615268d2853b7ed
augustine-awuori <codewithaugustine@gmail.com>
2024-09-12 12:11:27 +0300

    Bug fix: Update mailing

--- services/mailing.js
+++ services/mailing.js
@@ -1,27 +1,36 @@
-const mailer = require('nodemailer');
-const MailGen = require('mailgen');
+const mailer = require("nodemailer");
+const MailGen = require("mailgen");
 
 async function sendMail({ name, intro, to, subject }) {
-    const from = 'campuusmart@gmail.com';
+  const from = "campuusmart@gmail.com";
 
-    const htmlMail = new MailGen({ theme: 'default', name: 'Mailgen', link: 'https://mailgen.js/' }).
-        generate({
-            body: {
-                name, intro,
-                outro: `
+  const htmlMail = new MailGen({
+    theme: "default",
+    product: {
+      name: "Amazing",
+      link: "https://soamazing.shop/",
+    }
+  }).generate({
+    body: {
+      name,
+      intro,
+      outro: `
     Happy hustling!
     The Amazing Team
 
-    P.S. Don't forget, you can request any features or improvements you'd like to see – we're all ears!`
-            }
-        });
+    P.S. Don't forget, you can request any features or improvements you'd like to see – we're all ears!`,
+    },
+  });
 
-    return await mailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: from, pass: process.env.EMAIL_PASS
-        }
-    }).sendMail({ from, to, subject, html: htmlMail });
+  return await mailer
+    .createTransport({
+      service: "gmail",
+      auth: {
+        user: from,
+        pass: process.env.EMAIL_PASS,
+      },
+    })
+    .sendMail({ from, to, subject, html: htmlMail });
 }
 
-module.exports = { sendMail }
+module.exports = { sendMail };
