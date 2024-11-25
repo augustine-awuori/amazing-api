1570c9b66d227b7e73dbc070318b81629ac12a00
augustine-awuori <codewithaugustine@gmail.com>
2024-11-25 17:33:19 +0300

    Bug fix: Authorise for admin

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,18 +1,10 @@
-const _ = require("lodash");
 const service = require("../services/users");
 
 module.exports = async (req, res, next) => {
   const user = await service.findById(req.user._id);
 
   if (!user) return res.status(400).send({ error: "Invalid user" });
-  req.user = _.pick(user, [
-    "_id",
-    "name",
-    "username",
-    "avatar",
-    "aboutMe",
-    "coverPhoto",
-  ]);
+  req.user = user;
 
   next();
 };
